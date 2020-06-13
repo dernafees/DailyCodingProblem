0d2eec29d646ff3e888074623d4b606e79d5651e
Haider Naji <dernafees@hotmail.com>
2020-06-13 23:42:25 +0100

    add first problem without solution

---
+++ DailyCodingProblemProblem#329[Hard]/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DailyCodingProblemProblem_329_Hard_
+{
+    class Program
+    {
+
+        Dictionary<string,List<string>> guy_preferences = new Dictionary<string,List<string>>()
+        {
+            {"andrew" , new List<string>(){"caroline","abigail","betty"}},
+            {"bill" , new List<string>(){"caroline","betty","abigail"}},
+            {"andrew" , new List<string>(){"betty","caroline","abigail"}}
+        };
+          Dictionary<string,List<string>> gal_preferences = new Dictionary<string,List<string>>()
+        {
+            {"abigail" , new List<string>(){"andrew", "bill", "chester"}},
+            {"betty" , new List<string>(){"bill", "andrew", "chester"}},
+            {"carolin" , new List<string>(){"bill", "chester", "andrew"}}
+        };
+        static void Main(string[] args)
+        {
+
+            
+           
+
+
+
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DaolyCodingProblem
+# Daily Coding Problem
+
+This is a simple Repo that will include the daily coding problem soltuion with explenation and complexity.
+
+I recommend you visit this site if you are intersted and register https://www.dailycodingproblem.com/
