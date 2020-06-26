531593a2fa4bcebb0199cdefd7f95618279fd04e
Haider Naji <dernafees@hotmail.com>
2020-06-26 15:51:27 +0100

    add 374 problem and solution

---
+++ DailyCodingProblemProblem#374/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DailyCodingProblemProblem_374
+{
+    class Program
+    {
+        static int[] array = new int[] { -10, -1, 0, 2, 4, 11, 30, 50, 100 };
+
+        static void Main(string[] args)
+        {
+            OofNSolution();
+            Console.WriteLine($"The Value is { OLogNSoltuion(array, array.Length - 1, 0)}");
+        }
+
+        static int OLogNSoltuion(int[] array, int max, int min)
+        {
+            var mid = (max + min) / 2;
+            if (array[mid] == mid)
+            {
+                return mid;
+            }
+            if (array[mid] < mid)
+            {
+                return OLogNSoltuion(array, max, (mid + 1));
+            }
+            else
+            {
+                return OLogNSoltuion(array, (mid - 1), min);
+            }
+        }
+
+        static void OofNSolution()
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == i)
+                {
+                    Console.WriteLine($"The Value is {i}");
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DailyCodingProblemProblem#374/README.md
@@ -0,0 +1,16 @@
+Given a sorted array `arr` of distinct integers, return the lowest index `i` for which `arr[i] == i`. Return null if there is no such index.
+
+For example, given the array `[-5, -3, 2, 3]`, return `2` since `arr[2] == 2`. Even though `arr[3] == 3`, we return `2` since it's the lowest index.
+
+
+# Solution:
+So my approach to this was to brute force it which make the soltuion a linear one with O(N), which I left in the solution as a seperate method. what we need to achieve here is O(LogN)
+
+## Time Complexity:
+
+method `OLogNSoltuion` O(LogN)
+method `OofNSolution` O(N)
+
+
+
+
